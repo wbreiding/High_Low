f4c10094aefddbb9b382e0de93ee90b8200f598a
wbreiding <wdibean@gmail.com>
2016-01-20 17:23:09 -0800

    updated highlow game

--- highlow.php
+++ highlow.php
@@ -1,16 +1,18 @@
 <?php
-$number = rand(1,100);
+$number = mt_rand(1,100);
 $guess = 0;
+$numGuesses = 0;
 while ($guess != $number) {
   fwrite(STDOUT, 'Guess? ');
   $guess = fgets(STDIN);
-
+  $numGuesses++;
   if ($number > $guess) {
     fwrite(STDOUT, "HIGHER\n");
   } elseif ($number < $guess) {
     fwrite(STDOUT, "LOWER\n");
   } else {
     fwrite(STDOUT, "GOOD GUESS\n");
+    fwrite(STDOUT, "Number of guesses = $numGuesses\n");
   }
 }
  ?>
